036e2cd4336f9ca9545e7b80cc83fbb32fc102ba
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-25 00:50:55 +1300

    SRM 647 Div II 500 Travelling Salesman] Template code.

---
+++ srm647/div2/500_travelling_salesman/TravellingSalesmanEasy.cpp
@@ -0,0 +1,12 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "TravellingSalesmanEasy.cpp"
+#include <string>
+#include <vector>
+class TravellingSalesmanEasy {
+	public:
+	int getMaxProfit(int M, vector <int> profit, vector <int> city, vector <int> visit) {
+		
+	}
+};
---
+++ srm647/div2/500_travelling_salesman/TravellingSalesmanEasy.cs
@@ -0,0 +1,15 @@
+#line 5 "TravellingSalesmanEasy.cpp"
+#include <string>
+#include <vector>
+class TravellingSalesmanEasy {
+	public:
+	int getMaxProfit(int M, vector <int> profit, vector <int> city, vector <int> visit) {
+		
+	}
+};
+
+
+// Powered by FileEdit
+
+
+// Powered by FileEdit
