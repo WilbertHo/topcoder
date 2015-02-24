1ed5599f7cd735488b3a7f2b3982dedc3e97136d
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-25 00:44:46 +1300

    [SRM 650 Div 1 900 Legend of Adlez] Problem statement.

---
+++ srm650/div1/900_LegendOfAdlez/LegendOfAdlez.cpp
@@ -0,0 +1,12 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "LegendOfAdlez.cpp"
+#include <string>
+#include <vector>
+class LegendOfAdlez {
+	public:
+	long long safePatterns(vector <int> door1, vector <int> door2, vector <int> lock) {
+		
+	}
+};
---
+++ srm650/div1/900_LegendOfAdlez/LegendOfAdlez.java
@@ -0,0 +1,12 @@
+#line 5 "LegendOfAdlez.cpp"
+#include <string>
+#include <vector>
+class LegendOfAdlez {
+	public:
+	long long safePatterns(vector <int> door1, vector <int> door2, vector <int> lock) {
+		
+	}
+};
+
+
+// Powered by FileEdit
