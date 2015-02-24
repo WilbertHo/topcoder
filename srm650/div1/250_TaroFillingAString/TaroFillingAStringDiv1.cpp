8efdc7ef3b674fe177d58c383e0c444fd1054c8b
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-25 00:44:23 +1300

    [SRM 650 Div 1 250 Taro Filling A String] Template code.

---
+++ srm650/div1/250_TaroFillingAString/TaroFillingAStringDiv1.cpp
@@ -0,0 +1,12 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "TaroFillingAStringDiv1.cpp"
+#include <string>
+#include <vector>
+class TaroFillingAStringDiv1 {
+	public:
+	int getNumber(int N, vector <int> position, string value) {
+		
+	}
+};
---
+++ srm650/div1/250_TaroFillingAString/TaroFillingAStringDiv1.java
@@ -0,0 +1,15 @@
+#line 5 "TaroFillingAStringDiv1.cpp"
+#include <string>
+#include <vector>
+class TaroFillingAStringDiv1 {
+	public:
+	int getNumber(int N, vector <int> position, string value) {
+		
+	}
+};
+
+
+// Powered by FileEdit
+
+
+// Powered by FileEdit
