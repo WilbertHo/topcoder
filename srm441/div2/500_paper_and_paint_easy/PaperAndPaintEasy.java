6c37b2487b87a3e985aa3c02fdb887e655a31b7d
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-25 00:47:51 +1300

    [SRM 441 Div II 500 Paper and Paint] Template code.

---
+++ srm441/div2/500_paper_and_paint_easy/PaperAndPaintEasy.java
@@ -0,0 +1,12 @@
+#line 5 "PaperAndPaintEasy.cpp"
+#include <string>
+#include <vector>
+class PaperAndPaintEasy {
+	public:
+	long long computeArea(int width, int height, int xfold, int cnt, int x1, int y1, int x2, int y2) {
+		
+	}
+};
+
+
+// Powered by FileEdit
