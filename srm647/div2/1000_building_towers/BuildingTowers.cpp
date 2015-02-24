df97ebfe0282f832793a1ccfa9332172d30d9a8a
Wilbert Ho <wilbert.ho@gmail.com>
2015-02-25 00:51:46 +1300

    [SRM 647 Div II 1000 Building Towers] Template code.

---
+++ srm647/div2/1000_building_towers/BuildingTowers.cpp
@@ -0,0 +1,12 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "BuildingTowers.cpp"
+#include <string>
+#include <vector>
+class BuildingTowers {
+	public:
+	long long maxHeight(int N, int K, vector <int> x, vector <int> t) {
+		
+	}
+};
---
+++ srm647/div2/1000_building_towers/BuildingTowers.cs
@@ -0,0 +1,15 @@
+#line 5 "BuildingTowers.cpp"
+#include <string>
+#include <vector>
+class BuildingTowers {
+	public:
+	long long maxHeight(int N, int K, vector <int> x, vector <int> t) {
+		
+	}
+};
+
+
+// Powered by FileEdit
+
+
+// Powered by FileEdit
